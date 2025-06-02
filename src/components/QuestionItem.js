d4a2a570792b0ab050866d4424f436ed032acaf0
Caroline Ininga <carolineininga@gmail.com>
2025-06-02 23:05:44 +0300

    attempted crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => onDeleteQuestion(id));
+  }
+
+  function handleCorrectAnswerChange(e) {
+    const newCorrectIndex = parseInt(e.target.value, 10);
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex: newCorrectIndex }),
+    })
+      .then((r) => r.json())
+      .then(onUpdateQuestion);
+  }
 
   return (
     <li>
@@ -15,9 +26,19 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select
+          aria-label="Correct Answer"
+          value={correctIndex}
+          onChange={handleCorrectAnswerChange}
+        >
+          {answers.map((answer, index) => (
+            <option key={index} value={index}>
+              {answer}
+            </option>
+          ))}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
